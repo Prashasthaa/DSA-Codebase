0743bd98017d8c0174c60d5c72f0b5bd3d0550af
Prashastha <poojaryprashastha@gmail.com>
2026-01-03 06:34:44 +0000

    added sum of Subarray and total number of Subarray

--- Practice/Subarray.java
+++ Practice/Subarray.java
@@ -2,18 +2,24 @@ import java.util.*;
 
 public class Subarray{
     public static void printSubarrays(int num[]){
-
-for(int i =0;i<num.length;i++){
+    int   ts=0;
+    for(int i =0;i<num.length;i++){
+    
     int start =i;
     for(int j=i;j<num.length;j++){
         int end=j;
-        
+        int sum=0;
         for(int k=start;k<=end;k++){
             System.out.print(num[k]+" ");
+            sum+=num[k]; 
         }
+        ts++;
+        System.out.print(" Sum = "+sum);
         System.out.println();
     }
-}
+    
+    }
+    System.out.println("Total Subarray = "+ ts);
     }
     public static void main(String args[]){
         int numbers[]={2,4,6,8,10};
