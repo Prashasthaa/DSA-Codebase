5a21c45c62bb71efe86e12ba0175f47e6556420b
Prashastha <poojaryprashastha@gmail.com>
2026-01-04 09:51:45 +0530

    Maximum Subarray Sum- Prefix Sum

--- Practice/Subarray.java
+++ Practice/Subarray.java
@@ -22,8 +22,9 @@ public class Subarray {
         System.out.println("Total Subarray = " + ts);
     }
 
-    // bruteForce Apporach
-    public static void MaxSubarray(int num[]) {
+    // bruteForce Apporach Max Subarray Sum
+    // Time complexity=O(n^3)
+    public static void MaxSubarrayBrute(int num[]) {
         int curSum = 0;
         int MAX = Integer.MIN_VALUE;
 
@@ -43,11 +44,39 @@ public class Subarray {
         System.out.println("Maximum Subarray Sum = " + MAX);
     }
 
+    // Prefix Sum => Maximum Subarray Sum
+    // Time complexity=O(n^2)
+    public static void MaxSubarrayPrefix(int num[]) {
+        int curSum = 0;
+        int MaxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[num.length];
+
+        prefix[0] = num[0];
+        // calculalte prefix array
+        for (int i = 1; i < prefix.length; i++) {
+
+            prefix[i] = prefix[i - 1] + num[i];
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i; j < num.length; j++) {
+                curSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (curSum > MaxSum) {
+                    MaxSum = curSum;
+                }
+            }
+
+        }
+        System.out.println("Maximum Subarray Sum = " + MaxSum);
+    }
+
     public static void main(String args[]) {
         int numbers[] = { 2, 4, 6, 8, 10 };
 
-        printSubarrays(numbers);
+        // printSubarrays(numbers);
 
-        MaxSubarray(numbers);
+        // MaxSubarrayBrute(numbers);
+
+        MaxSubarrayPrefix(numbers);
     }
 }
