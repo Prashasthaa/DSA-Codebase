ab4eeb4d3f5181a85e268e10616cc32fe05a1914
Prashastha <poojaryprashastha@gmail.com>
2026-01-16 09:55:32 +0530

    Selection Sort

--- Practice/BasicSorting.java
+++ Practice/BasicSorting.java
@@ -9,21 +9,45 @@ public class BasicSorting {
 
     public static void bubbleSort(int arr[]) {
         for (int turn = 0; turn < arr.length - 1; turn++) {
+            int swap = 0;
             for (int j = 0; j < arr.length - 1 - turn; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swap++;
                 }
 
             }
+            if (swap == 0) {
+                return;
+            }
+        }
+    }
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+
         }
     }
 
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
+        // int arr[] = { 1, 2, 3, 4, 5 };
         printArray(arr);
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        selectionSort(arr);
         printArray(arr);
     }
 
