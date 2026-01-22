0a82e9b3a5e0ec7626767bdf3e9ee18fe3b27fa4
Prashastha <poojaryprashastha@gmail.com>
2026-01-22 12:24:53 +0530

    Search in Sorted matrix

--- Practice/Matrices.java
+++ Practice/Matrices.java
@@ -92,6 +92,56 @@ public class Matrices {
         System.out.println("Diagonal sum =" + sum);
     }
 
+    // Search in sorted matrix
+    // time compelxity=(n+m)
+    public static void searchInSortedMatrix(int matrix[][], int key) {
+        int col = matrix.length - 1;
+        int row = 0;
+        while (col >= 0 && row <= matrix.length) {
+            if (key == matrix[row][col]) {
+                System.out.println("found key at ( " + row + " , " + col + " ) ");
+                return;
+            } else if (key > matrix[row][col]) {
+                row++;
+            } else if (key < matrix[row][col]) {
+                col--;
+            }
+        }
+        System.out.println("Key is not found");
+    }
+
+    // practice
+
+    public static void count(int matrix[][], int num) {
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (num == matrix[i][j]) {
+                    count += 1;
+                }
+            }
+        }
+        System.out.println("Count = " + count);
+    }
+
+    public static void sumOfRow(int matrix[][], int row) {
+        int sum = 0;
+        for (int i = 0; i < matrix[0].length; i++) {
+            sum += matrix[row][i];
+        }
+        System.out.println("sum of the row is " + sum);
+    }
+
+    public static void transpose(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
         int matrix1[][] = new int[3][3];
         int n = 3, m = 3;
@@ -114,16 +164,30 @@ public class Matrices {
             }
             System.out.println();
         }
+
+        transpose(matrix1);
         search(matrix1, 9);
 
-        int matrix[][] = { { 1, 2, 3, 4 },
+        int matrix2[][] = { { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
                 { 13, 14, 15, 16 } };
 
-        printSpiral(matrix);
+        printSpiral(matrix2);
 
-        diagonalSum(matrix);
+        diagonalSum(matrix2);
+
+        int matrix3[][] = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 }, { 27, 29, 37, 48 }, { 32, 33, 39, 50 } };
+        int key = 33;
+        searchInSortedMatrix(matrix3, key);
+
+        int matrix5[][] = { { 4, 7, 9 }, { 8, 8, 7 } };
+        int num = 7;
+        count(matrix5, num);
+
+        int[][] matrix6 = { { 1, 4, 9 }, { 11, 4, 3 }, { 2, 2, 3 } };
+        int row = 1;
+        sumOfRow(matrix6, row);
     }
 
 }
