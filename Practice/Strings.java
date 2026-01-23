f3b31115ba4e1a5a5b2fe7de0b4905b85cc55695
Prashastha <poojaryprashastha@gmail.com>
2026-01-23 15:47:29 +0530

    String

---
+++ Practice/Strings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Strings {
+
+    public static void main(String[] args) {
+
+        // // Defining strings
+        // char arr[] = { 'a', 'b', 'c', 'd' };
+        // String str = "abcd";
+        // String str1 = new String("xyz");
+
+        // // In java strings are immutable
+
+        // // Input the strings
+        // Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+
+        // // output the string
+        // System.out.println(name);
+
+        // Length of string
+        String fullName = "Prashastha Poojary";
+        System.out.println(fullName.length());
+
+    }
+
+}
