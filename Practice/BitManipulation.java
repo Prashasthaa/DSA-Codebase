f7ad047066e0420738734e55d52315e82a7c72a4
Prashastha <poojaryprashastha@gmail.com>
2026-01-29 05:23:14 +0530

    Bit Operators

---
+++ Practice/BitManipulation.java
@@ -0,0 +1,24 @@
+public class BitManipulation {
+    public static void main(String[] args) {
+
+        // binary operators
+
+        // Binary And operator
+        System.out.println(5 & 6);
+
+        // Binary OR operator
+        System.out.println(5 | 6);
+
+        // Binary XOR OPERATOR
+        System.out.println(5 ^ 6);
+
+        // Binary one's complement
+        System.out.println(~5);
+
+        // Binary left shift
+        System.out.println(5 << 2);
+
+        // Binary Right shift
+        System.out.println(7 >> 2);
+    }
+}
