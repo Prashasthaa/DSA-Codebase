a04826dfe239b6f17aad4a64ac6758c2ccc92cbc
Prashastha <poojaryprashastha@gmail.com>
2026-02-01 10:37:24 +0530

    access specifers

--- Practice/OOPS.java
+++ Practice/OOPS.java
@@ -9,6 +9,31 @@ public class OOPS {
 
         p1.setTip(6);
         System.out.println(p1.tip);
+
+        BankAccount myAccount = new BankAccount();
+        myAccount.username = "prashastha";
+
+        // // error due to The field BankAccount.password is not visible it has the
+        // access
+        // // specifier as private
+        // myAccount.password = "password";
+
+        myAccount.setPassword("newPassword");
+    }
+}
+
+// ==> 4 types of access specifiers
+// 1)Private
+// 2)Public
+// 3)Protected
+// 4)Default
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+        password = pwd;
     }
 }
 
