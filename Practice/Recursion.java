37cc8ac37bd5a1ef75d65d905ba9473cdae8916e
Prashastha <poojaryprashastha@gmail.com>
2026-02-06 06:13:01 +0530

    sum of n natural numbers

--- Practice/Recursion.java
+++ Practice/Recursion.java
@@ -16,10 +16,26 @@ public class Recursion {
         System.out.println(n);
     }
 
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial((n - 1));
+    }
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return n;
+        }
+        return n + sum(n - 1);
+    }
+
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         // printDecreasingNum(n);
-        printIncreasingNum(n);
+        // printIncreasingNum(n);
+        System.out.println(factorial(n));
+        System.out.println(sum(n));
 
     }
 
