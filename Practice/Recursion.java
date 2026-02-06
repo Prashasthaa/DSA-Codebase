277920622f9c019c47bd0b261b6f8105a2ed39c7
Prashastha <poojaryprashastha@gmail.com>
2026-02-06 05:51:36 +0530

    print increasing number

--- Practice/Recursion.java
+++ Practice/Recursion.java
@@ -7,9 +7,20 @@ public class Recursion {
         printDecreasingNum(n - 1);
     }
 
+    public static void printIncreasingNum(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        printIncreasingNum(n - 1);
+        System.out.println(n);
+    }
+
     public static void main(String[] args) {
         int n = 10;
-        printDecreasingNum(n);
+        // printDecreasingNum(n);
+        printIncreasingNum(n);
+
     }
 
 }
