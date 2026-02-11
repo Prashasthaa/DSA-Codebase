de46bb6ac892ae629b4279f3f415b706694b73c2
Prashastha <poojaryprashastha@gmail.com>
2026-02-11 06:30:24 +0530

    remove duplicate from string

--- Practice/Recursion.java
+++ Practice/Recursion.java
@@ -117,6 +117,26 @@ public class Recursion {
         return toWays;
     }
 
+    // Remove duplicates in a string
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]) {
+        // base Case
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+
+        // work
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            // duplicate
+            removeDuplicate(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, idx + 1, newStr.append(currChar), map);
+        }
+
+    }
+
     public static void main(String[] args) {
         int n = 5;
         // printDecreasingNum(n);
@@ -136,7 +156,10 @@ public class Recursion {
         // System.out.println(powerOfX(5, 1));
         // System.out.println(optimizedPower(8, 2));
 
-        System.out.println(tilingProblem(4));
+        // System.out.println(tilingProblem(4));
+
+        String str = "prashastha";
+        removeDuplicate(str, 0, new StringBuilder(""), new boolean[26]);
     }
 
 }
