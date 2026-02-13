48889e2fef2fa97b3205c14a766c725e6bac9eab
Prashastha <poojaryprashastha@gmail.com>
2026-02-13 05:46:51 +0530

    convert number to string

--- Practice/Recursion.java
+++ Practice/Recursion.java
@@ -186,6 +186,45 @@ public class Recursion {
 
     }
 
+    public static void convertToString(int n) {
+        if (n == 0) {
+            return;
+        }
+        int digit = n % 10;
+        convertToString(n / 10);
+        printDigit(digit);
+
+    }
+
+    public static void printDigit(int n) {
+        if (n == 0) {
+            System.out.print("zero ");
+        } else if (n == 1) {
+            System.out.print("one ");
+
+        } else if (n == 2) {
+            System.out.print("two ");
+        } else if (n == 3) {
+            System.out.print("three ");
+
+        } else if (n == 4) {
+            System.out.print("four ");
+        } else if (n == 5) {
+            System.out.print("five ");
+
+        } else if (n == 6) {
+            System.out.print("six ");
+
+        } else if (n == 7) {
+            System.out.print("seven ");
+
+        } else if (n == 8) {
+            System.out.print("eight ");
+        } else if (n == 9) {
+            System.out.print("nine ");
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         // printDecreasingNum(n);
@@ -214,8 +253,10 @@ public class Recursion {
 
         // printBinaryString(3, 0, "");
 
-        int nums[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
-        allOccurrence(nums, 2, 0);
+        // int nums[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
+        // allOccurrence(nums, 2, 0);
+
+        convertToString(4567);
     }
 
 }
