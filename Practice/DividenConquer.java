8d27c298f669cce8fa617f6e211336f02a5da10f
Prashastha <poojaryprashastha@gmail.com>
2026-02-17 19:26:22 +0530

    quick sort

--- Practice/DividenConquer.java
+++ Practice/DividenConquer.java
@@ -48,10 +48,44 @@ public class DividenConquer {
         }
     }
 
+    public static void quickSort(int arr[], int si, int ei) {
+
+        // base Case
+        if (si >= ei) {
+            return;
+        }
+
+        int partIndex = parition(arr, si, ei);
+        quickSort(arr, si, partIndex - 1);
+        quickSort(arr, partIndex + 1, ei);
+
+    }
+
+    public static int parition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+
     public static void main(String args[]) {
         int arr[] = { 6, 3, 9, 5, 2, 8 };
         printArr(arr);
-        mergeSort(arr, 0, arr.length - 1);
+        // mergeSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
         printArr(arr);
     }
 }
