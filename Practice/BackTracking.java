7725ab66dc85b6a2e0d8c5a3c511576ae7779101
Prashastha <poojaryprashastha@gmail.com>
2026-03-25 20:20:22 +0530

    sudoko solver

--- Practice/BackTracking.java
+++ Practice/BackTracking.java
@@ -200,6 +200,79 @@ public class BackTracking {
         return w1 + w2;
     }
 
+    public static void printBoard(int board[][]) {
+        System.out.println("--------Sudoko---------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static boolean isSafeSudoku(int sudoko[][], int row, int col, int digit) {
+
+        // column
+        for (int i = 0; i <= 8; i++) {
+            if (sudoko[i][col] == digit) {
+                return false;
+            }
+        }
+
+        // row
+        for (int j = 0; j <= 8; j++) {
+            if (sudoko[row][j] == digit) {
+                return false;
+            }
+        }
+
+        // grid
+
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoko[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean sudokoSolver(int sudoko[][], int row, int col) {
+
+        // basecase
+        if (row == 9) {
+            return true;
+        }
+
+        // recursion
+        int newRow = row, newCol = col + 1;
+        if (col + 1 == 9) {
+            newRow = row + 1;
+            newCol = 0;
+        }
+
+        if (sudoko[row][col] != 0) {
+            return sudokoSolver(sudoko, newRow, newCol);
+        }
+
+        for (int digit = 1; digit <= 9; digit++) {
+            if (isSafeSudoku(sudoko, row, col, digit)) {
+                sudoko[row][col] = digit;
+                if (sudokoSolver(sudoko, newRow, newCol)) {
+                    return true;
+                }
+                sudoko[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
@@ -228,7 +301,24 @@ public class BackTracking {
 
         // nKnights(board, 0);
 
-        int n = 4, m = 4;
-        System.out.println("total grid ways = " + gridWays(0, 0, n, m));
+        // int n = 4, m = 4;
+        // System.out.println("total grid ways = " + gridWays(0, 0, n, m));
+
+        int sudoku[][] = { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
+                { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
+                { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
+                { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
+                { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
+                { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
+                { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
+                { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
+                { 8, 2, 7, 0, 0, 9, 0, 1, 3 } };
+
+        if (sudokoSolver(sudoku, 0, 0)) {
+            printBoard(sudoku);
+
+        } else {
+            System.out.println("Solution Does not exist ");
+        }
     }
 }
