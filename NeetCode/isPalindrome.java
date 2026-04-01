14e8c1df2ed314121a3766165d1e165838a02a1f
Prashastha <poojaryprashastha@gmail.com>
2026-04-01 15:39:52 +0530

    valid palindrome

---
+++ NeetCode/isPalindrome.java
@@ -0,0 +1,24 @@
+package NeetCode;
+
+public class isPalindrome {
+    public boolean isPalindrome(String s) {
+        int lp = 0;
+        int rp = s.length() - 1;
+
+        while (lp < rp) {
+            while (lp < rp && !Character.isLetterOrDigit(s.charAt(lp))) {
+                lp++;
+            }
+            while (lp < rp && !Character.isLetterOrDigit(s.charAt(rp))) {
+                rp--;
+            }
+            if (Character.toLowerCase(s.charAt(lp)) != Character.toLowerCase(s.charAt(rp))) {
+                return false;
+
+            }
+            rp--;
+            lp++;
+        }
+        return true;
+    }
+}
