dd389c55883d64597f74016ff50cced072ccbd19
Prashastha <poojaryprashastha@gmail.com>
2026-04-15 22:29:09 +0530

    Linked List

--- Practice/LinkedList.java
+++ Practice/LinkedList.java
@@ -11,10 +11,12 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data) {
         // step 1=> create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -25,12 +27,13 @@ public class LinkedList {
 
         // step 3=head=new node
         head = newNode;
+
     }
 
     public void addLast(int data) {
         // step-1 create new node
         Node newNode = new Node(data);
-
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -41,6 +44,7 @@ public class LinkedList {
 
         // step-3 tail node =new node
         tail = newNode;
+
     }
 
     public void print() { // O(n)
@@ -57,6 +61,114 @@ public class LinkedList {
         System.err.println("null");
     }
 
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i=idx-1 temp->prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("linked list is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev:idx=size-2
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; // tail.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+
+    }
+
+    public int itrSearch(int key) {
+        int i = 0;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        head = prev;
+    }
+
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
@@ -68,6 +180,21 @@ public class LinkedList {
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
+        ll.print();
+        ll.add(2, 9);
+        ll.print();
+        System.out.println(ll.size);
+        System.out.println(ll.removeFirst());
+        System.out.println(ll.size);
+        ll.print();
+        System.out.println(ll.removeLast());
+        System.out.println(ll.size);
+        ll.print();
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.itrSearch(7));
+        System.out.println(ll.recSearch(3));
+        System.out.println(ll.recSearch(7));
+        ll.reverse();
         ll.print();
     }
 }
