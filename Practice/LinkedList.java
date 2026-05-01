4ccee5f41f9c42ff52975e8b32dce053133834b3
Prashastha <poojaryprashastha@gmail.com>
2026-05-01 11:42:08 +0530

    zig Zag

--- Practice/LinkedList.java
+++ Practice/LinkedList.java
@@ -333,6 +333,46 @@ public class LinkedList {
         return merge(newLeft, newRight);
     }
 
+    public void zigZag() {
+        // 1.find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node mid = slow;
+
+        // 2.reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // 3.alternate and merging-> zig-zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            right = nextR;
+            left = nextL;
+        }
+
+    }
+
     public static void main(String[] args) {
 
         // LinkedList ll = new LinkedList();
@@ -392,7 +432,8 @@ public class LinkedList {
 
         ll.print();
 
-        ll.head = ll.mergeSort(ll.head);
+        // ll.head = ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
     }
 }
