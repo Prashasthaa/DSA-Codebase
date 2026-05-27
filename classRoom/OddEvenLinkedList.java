e6191363be1ada0f5bab1415d00e80890a2c8f4e
Prashastha <poojaryprashastha@gmail.com>
2026-05-27 22:44:15 +0530

    Segregate Evens and Odds in a Linked List

---
+++ classRoom/OddEvenLinkedList.java
@@ -0,0 +1,47 @@
+package classRoom;
+
+/* Structure of a link list node
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class OddEvenLinkedList {
+    Node divide(Node head) {
+        // code here
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node odd = new Node(-1);
+        Node even = new Node(-1);
+
+        Node ans = even;
+        Node oddHead = odd;
+
+        Node ptr = head;
+
+        while (ptr != null) {
+            if (ptr.data % 2 == 0) {
+                even.next = ptr;
+                even = even.next;
+            } else {
+                odd.next = ptr;
+                odd = odd.next;
+            }
+            ptr = ptr.next;
+        }
+
+        even.next = null;
+        odd.next = null;
+
+        even.next = oddHead.next;
+
+        return ans.next;
+    }
+}
