4661d79ae7ede4a432cf85558cd70e2483ed04d2
Prashastha <poojaryprashastha@gmail.com>
2026-06-02 10:21:41 +0530

    Stack using LinkedList

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -34,9 +34,71 @@ public class StackB {
 
     }
 
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class StackLinkedList {
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // push
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+
+        }
+
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+
+        }
+    }
+
     public static void main(String args[]) {
 
-        StackArrayList s = new StackArrayList();
+        // StackArrayList s = new StackArrayList();
+
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // while (!s.isEmpty()) {
+        // System.out.println(s.peek());
+        // s.pop();
+        // }
+
+        StackLinkedList s = new StackLinkedList();
 
         s.push(1);
         s.push(2);
@@ -46,6 +108,5 @@ public class StackB {
             System.out.println(s.peek());
             s.pop();
         }
-
     }
 }
