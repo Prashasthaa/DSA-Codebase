dfa872a5ab45e6144a434b06a56469fb4e9d5cbb
Prashastha <poojaryprashastha@gmail.com>
2026-06-02 12:30:49 +0530

    Revese a Stack

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -112,6 +112,23 @@ public class StackB {
         return result.toString();
     }
 
+    public static void printStack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
     public static void main(String args[]) {
 
         // StackArrayList s = new StackArrayList();
@@ -159,7 +176,15 @@ public class StackB {
         // s.pop();
         // }
 
-        System.out.println(reverseString("hello"));
+        // System.out.println(reverseString("hello"));
+
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        reverseStack(s);
+        printStack(s);
 
     }
 }
