8e09325f9cc7c75e95e7f76413791bba50212772
Prashastha <poojaryprashastha@gmail.com>
2026-06-02 12:01:54 +0530

    Push at bottom of stack

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -85,6 +85,17 @@ public class StackB {
         }
     }
 
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+
+    }
+
     public static void main(String args[]) {
 
         // StackArrayList s = new StackArrayList();
@@ -109,15 +120,28 @@ public class StackB {
         // s.pop();
         // }
 
-        Stack<Integer> s = new Stack<>();
+        // Stack<Integer> s = new Stack<>();
 
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // while (!s.isEmpty()) {
+        // System.out.println(s.peek());
+        // s.pop();
+        // }
+
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
+
+        pushAtBottom(s, 4);
 
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
+
     }
 }
