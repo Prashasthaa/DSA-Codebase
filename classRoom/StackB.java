effb7380a030694d3674d060d9648529dd7385a8
Prashastha <poojaryprashastha@gmail.com>
2026-06-02 12:11:07 +0530

    Revese a String using Stack

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -96,6 +96,22 @@ public class StackB {
 
     }
 
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
     public static void main(String args[]) {
 
         // StackArrayList s = new StackArrayList();
@@ -131,17 +147,19 @@ public class StackB {
         // s.pop();
         // }
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        pushAtBottom(s, 4);
+        // pushAtBottom(s, 4);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
+        // while (!s.isEmpty()) {
+        // System.out.println(s.peek());
+        // s.pop();
+        // }
+
+        System.out.println(reverseString("hello"));
 
     }
 }
