3a6a13bf8175aee491cdc77ff6ec30d1039cf9c1
Prashastha <poojaryprashastha@gmail.com>
2026-06-05 10:47:01 +0530

    Stock span Problem

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -129,6 +129,27 @@ public class StackB {
         pushAtBottom(s, top);
     }
 
+    public static void stockSpan(int[] stocks, int[] span) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            s.push(i);
+
+        }
+    }
+
     public static void main(String args[]) {
 
         // StackArrayList s = new StackArrayList();
@@ -178,13 +199,22 @@ public class StackB {
 
         // System.out.println(reverseString("hello"));
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        reverseStack(s);
-        printStack(s);
+        // reverseStack(s);
+        // printStack(s);
+
+        // Question 4 stock span problem
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.println(span[i] + " ");
+        }
 
     }
 }
