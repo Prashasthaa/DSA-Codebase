3c3b16270f573c64497189838d9385c03c921a55
Prashastha <poojaryprashastha@gmail.com>
2026-07-02 22:06:09 +0530

    minstack leetcode 155

---
+++ NeetCode/MinStack.java
@@ -0,0 +1,41 @@
+package NeetCode;
+
+class MinStack {
+    private Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int value) {
+        if (head == null) {
+            head = new Node(value, value, null);
+        } else {
+            head = new Node(value, Math.min(value, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val, min;
+        Node next;
+
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
