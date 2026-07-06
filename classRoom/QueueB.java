aa1f37e2382fda943f50071b4161883add193a25
Prashastha <poojaryprashastha@gmail.com>
2026-07-06 22:17:32 +0530

    implementation of queue using linked list

--- classRoom/QueueB.java
+++ classRoom/QueueB.java
@@ -1,37 +1,34 @@
 public class QueueB {
+    /* Queue using linked list */
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
 
     static class Queue {
-        static int arr[];
-        static int size;
-        static int rear;
-        static int front;
 
-        Queue(int n) {
-            arr = new int[n];
-            size = n;
-            rear = -1;
-            front = -1;
-        }
+        static Node head = null;
+        static Node tail = null;
 
         public static boolean isEmpty() {
-            return rear == -1 && front == -1;
-        }
-
-        public static boolean isFull() {
-            return (rear + 1) % size == front;
+            return head == null && tail == null;
         }
 
         // add
         public static void add(int data) {
-            if (isFull()) {
-                System.out.println("Queue is full");
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
                 return;
             }
-            if (front == -1) {
-                front = 0;
-            }
-            rear = (rear + 1) % size;
-            arr[rear] = data;
+            tail.next = newNode;
+            tail = newNode;
         }
 
         // remove
@@ -40,14 +37,13 @@ public class QueueB {
                 System.out.println("empty queue");
                 return -1;
             }
-            int result = arr[front];
-            front = (front + 1) % size;
-            if (rear == front) {
-                rear = front = -1;
+            int front = head.data;
+            if (tail == head) {
+                tail = head = null;
             } else {
-                front = (front + 1) % size;
+                head = head.next;
             }
-            return result;
+            return front;
 
         }
 
@@ -58,18 +54,77 @@ public class QueueB {
                 System.out.println("empty queue");
                 return -1;
             }
-            return arr[front];
+            return head.data;
         }
     }
+    /* Queue using array */
+    // static class Queue {
+    // static int arr[];
+    // static int size;
+    // static int rear;
+    // static int front;
+
+    // Queue(int n) {
+    // arr = new int[n];
+    // size = n;
+    // rear = -1;
+    // front = -1;
+    // }
+
+    // public static boolean isEmpty() {
+    // return rear == -1 && front == -1;
+    // }
+
+    // public static boolean isFull() {
+    // return (rear + 1) % size == front;
+    // }
+
+    // // add
+    // public static void add(int data) {
+    // if (isFull()) {
+    // System.out.println("Queue is full");
+    // return;
+    // }
+    // if (front == -1) {
+    // front = 0;
+    // }
+    // rear = (rear + 1) % size;
+    // arr[rear] = data;
+    // }
+
+    // // remove
+    // public static int remove() {
+    // if (isEmpty()) {
+    // System.out.println("empty queue");
+    // return -1;
+    // }
+    // int result = arr[front];
+    // front = (front + 1) % size;
+    // if (rear == front) {
+    // rear = front = -1;
+    // } else {
+    // front = (front + 1) % size;
+    // }
+    // return result;
+
+    // }
+
+    // // peek
+    // public static int peek() {
+
+    // if (isEmpty()) {
+    // System.out.println("empty queue");
+    // return -1;
+    // }
+    // return arr[front];
+    // }
+    // }
 
     public static void main(String[] args) {
-        Queue q = new Queue(3);
+        Queue q = new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
-        System.out.println(q.remove());
-        q.add(4);
-        System.out.println(q.remove());
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
