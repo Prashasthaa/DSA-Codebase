3d9e13822ddf63afe0a03fec772b8af62367e4d6
Prashastha <poojaryprashastha@gmail.com>
2026-07-07 11:54:50 +0530

    implementation of stack using 2 queues

--- classRoom/StackB.java
+++ classRoom/StackB.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class StackB {
     static class StackArrayList {
@@ -82,6 +83,71 @@ public class StackB {
             }
             return head.data;
 
+        }
+    }
+
+    static class StackQueue {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public static void push(int data) {
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                System.out.println("empty stack");
+                return -1;
+            }
+            int top = -1;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty stack");
+                return -1;
+            }
+            int top = -1;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+
+                    q1.add(top);
+                }
+            }
+            return top;
         }
     }
 
@@ -404,8 +470,18 @@ public class StackB {
         // String path = "/home/user/Documents/../Pictures";
         // System.out.println(simplifyPath(path));
 
-        // Question -10 => longest valid parenthess lenght
-        String s = "(()";
-        System.out.println(longestValidParentheses(s));
+        // // Question -10 => longest valid parenthess lenght
+        // String s = "(()";
+        // System.out.println(longestValidParentheses(s));
+
+        StackQueue s = new StackQueue();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
     }
 }
