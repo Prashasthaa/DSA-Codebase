1a108c71a67e0955b313ab374e09b92e01fdf6fa
Prashastha <poojaryprashastha@gmail.com>
2026-07-15 19:53:06 +0530

    activity selection

--- classRoom/greeedyAlgo.java
+++ classRoom/greeedyAlgo.java
@@ -1,19 +1,33 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class greeedyAlgo {
 
     public static void activitySelection(int[] start, int[] end) {
+        // sorting
+        int activities[][] = new int[start.length][3];
+
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
+
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
 
         for (int i = 1; i < end.length; i++) {
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
         System.out.println("max activities=" + maxAct);
