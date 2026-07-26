508a682f7c5d4c6d5d574975bafdec68aced3517
Prashastha <poojaryprashastha@gmail.com>
2026-07-26 19:07:10 +0530

    Add methods to check if a tree is identical and if one tree is a subtree of another

--- classRoom/BinaryTreesB.java
+++ classRoom/BinaryTreesB.java
@@ -161,6 +161,36 @@ public class BinaryTreesB {
         return new Info(diam, ht);
     }
 
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+
+        if (!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
@@ -198,6 +228,8 @@ public class BinaryTreesB {
         Node subRoot = new Node(2);
         subRoot.left = new Node(4);
         subRoot.right = new Node(5);
+
+        System.out.println(isSubtree(root, subRoot));
 
     }
 }
