7c2f2207a81160f470da33d4264b769ccc61a865
Prashastha <poojaryprashastha@gmail.com>
2026-07-28 20:13:26 +0530

    lowest common ancestor

--- classRoom/BinaryTreesB.java
+++ classRoom/BinaryTreesB.java
@@ -256,6 +256,45 @@ public class BinaryTreesB {
         KLevel(root.right, level + 1, k);
     }
 
+    // Lowest common ancestor approach -1
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        // last equal node->i-1th4
+        Node lca = path1.get((i - 1));
+        return lca;
+    }
+
     public static void main(String[] args) {
         // int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
@@ -305,7 +344,8 @@ public class BinaryTreesB {
         root.right.right = new Node(7);
         // topView(root);
 
-        KLevel(root, 1, 2);
-
+        // KLevel(root, 1, 2);
+        int n1 = 4, n2 = 7;
+        System.out.println(lca(root, n1, n2).data);
     }
 }
