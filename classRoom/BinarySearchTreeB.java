be774e8d833cbf9f2f94a6c60a019ecd489242c7
Prashastha <poojaryprashastha@gmail.com>
2026-08-04 22:30:10 +0530

    Deletion of a Node in binary search tree

--- classRoom/BinarySearchTreeB.java
+++ classRoom/BinarySearchTreeB.java
@@ -32,7 +32,7 @@ public class BinarySearchTreeB {
             return;
         }
         inorder(root.left);
-        System.out.println(root.data);
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
 
@@ -50,8 +50,39 @@ public class BinarySearchTreeB {
         }
     }
 
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case-1 leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case-2 single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // case-3 - both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -59,6 +90,8 @@ public class BinarySearchTreeB {
         }
         inorder(root);
         System.out.println(search(root, 7));
+        delete(root, 10);
+        inorder(root);
 
     }
 }
